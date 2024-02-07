1d16bb42c4e1ac57c5aa7de2f02785b5b76e9ad4
李海清 <LyReonn@icloud.com>
2024-02-07 15:48:05 +0800

    list file test

--- script.js
+++ script.js
@@ -10,11 +10,27 @@ window.onload = function () {
         playlist.appendChild(tracks[n]);
     }
 
+    var fileNames = new Array();
+    $.ajax({
+        url: "/",
+        success: function (data) {
+            $(data).find("td > a").each(function () {
+                if (openFile($(this).attr("href"))) {
+                    fileNames.push($(this).attr("href"));
+                }
+            });
+        }
+    });
+    console.log(fileNames);
 }
 
-$(document).ready(function () {
-    $.get(".", function (allFiles) {
-        $("#fileNames").append(allFiles);
-        console.log(allFiles);
-    });
-})
+function openFile(file) {
+    var extension = file.substr((file.lastIndexOf('.') + 1));
+    switch (extension) {
+        case 'mp3':
+        case 'mp4':
+            return true;
+        default:
+            return false;
+    }
+};
