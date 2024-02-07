56744b9d1604290751886c50bd5f40581a3f17ee
李海清 <LyReonn@icloud.com>
2024-02-07 22:17:56 +0800

    update

--- script.js
+++ script.js
@@ -1,27 +1,43 @@
 "use strict";
 
+var folder = "./assets/showcase/";
+var plwrap, player, source;
 var tracks = [];
 
 window.onload = function () {
-    var playlist = document.getElementById("playlist");
-    
-    fetch("./filelist.json")
-        .then((response) => response.json())
-        .then((jsondata) => writeplaylist(jsondata, playlist));
-
     // var url = "https://api.github.com/repos/lyreonn/showcase/contents/assets/showcase/";
     // (async () => {
     //     const response = await fetch(url);
     //     const jsondata = await response.json();
     //     writeplaylist(jsondata, playlist);
     // })()
+
+    plwrap = document.getElementById("playlist");
+    player = document.getElementById("player");
+    source = document.createElement("source");
+
+    fetch("./filelist.json")
+        .then((response) => response.json())
+        .then((jsondata) => writeplaylist(jsondata));
+
+    source.setAttribute("src", folder + "PV声音设计-双生幻想.mp4");
+    player.appendChild(source);
 }
 
-function writeplaylist(jsondata, playlist) {
+function writeplaylist(jsondata) {
     for (var n = 0; n < jsondata.length; n++) {
         tracks[n] = document.createElement("div");
-        tracks[n].className = "tracks";
         tracks[n].innerHTML = jsondata[n].name;
-        playlist.appendChild(tracks[n]);
+        tracks[n].className = "tracks";
+        tracks[n].setAttribute("id", n);
+        tracks[n].addEventListener("click", changeplaying);
+        plwrap.appendChild(tracks[n]);
     }
+}
+
+function changeplaying() {
+    player.removeChild(source);
+    source.removeAttribute("src");
+    source.setAttribute("src", folder + this.innerHTML);
+    player.appendChild(source);
 }
