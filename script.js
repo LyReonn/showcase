281d4eaf642c57410f0ea52484ea623e9d0e8191
李海清 <LyReonn@icloud.com>
2024-02-07 16:30:18 +0800

    structure redesign

--- script.js
+++ script.js
@@ -10,27 +10,10 @@ window.onload = function () {
         playlist.appendChild(tracks[n]);
     }
 
-    var fileNames = new Array();
-    $.ajax({
-        url: "/",
-        success: function (data) {
-            $(data).find("td > a").each(function () {
-                if (openFile($(this).attr("href"))) {
-                    fileNames.push($(this).attr("href"));
-                }
-            });
-        }
-    });
-    console.log(fileNames);
-}
+    (async () => {
+        const response = await fetch('https://api.github.com/repos/lyreonn/showcase/contents/');
+        const data = await response.json();
+        console.log(data);
+    })()
 
-function openFile(file) {
-    var extension = file.substr((file.lastIndexOf('.') + 1));
-    switch (extension) {
-        case 'mp3':
-        case 'mp4':
-            return true;
-        default:
-            return false;
-    }
-};
+}
