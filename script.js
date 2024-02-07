ca4fc42438bb335d866c1c21d073b7483c909e24
李海清 <LyReonn@icloud.com>
2024-02-07 20:47:48 +0800

    update

--- script.js
+++ script.js
@@ -1,24 +1,23 @@
 "use strict";
 
-/*
-var url = "https://api.github.com/repos/lyreonn/showcase/contents/assets/showcase/";
-(async () => {
-    const response = await fetch(url);
-    const jsondata = await response.json();
-    writeplaylist(jsondata);
-})()
-*/
-
 var tracks = [];
 
 window.onload = function () {
     var playlist = document.getElementById("playlist");
+    
     fetch("./filelist.json")
         .then((response) => response.json())
-        .then((jsondata) => writeplaylist(jsondata));
+        .then((jsondata) => writeplaylist(jsondata, playlist));
+
+    // var url = "https://api.github.com/repos/lyreonn/showcase/contents/assets/showcase/";
+    // (async () => {
+    //     const response = await fetch(url);
+    //     const jsondata = await response.json();
+    //     writeplaylist(jsondata, playlist);
+    // })()
 }
 
-function writeplaylist(jsondata) {
+function writeplaylist(jsondata, playlist) {
     for (var n = 0; n < jsondata.length; n++) {
         tracks[n] = document.createElement("div");
         tracks[n].className = "tracks";
